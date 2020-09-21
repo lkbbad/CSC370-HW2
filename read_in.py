4220f107b3ffcd21456d48740f2249ca4c44e533
Your Name <casigl@davidson.edu>
2020-09-21 19:29:11 -0400

    up to date read_in method

--- read_in.py
+++ read_in.py
@@ -7,21 +7,21 @@ import pandas
 from pandas import read_csv
 
 #Importing Dataset #1
-url = "https://raw.githubusercontent.com/casigl/Datasets/master/dataset1.csv"
+url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset1.csv"
 dataset1_df = read_csv(url)
 dataset1_df.columns = ['x', 'f(x)']
 
 print(dataset1_df)
 
 #Importing Dataset #2
-url = "https://raw.githubusercontent.com/casigl/Datasets/master/dataset2.csv"
+url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset2.csv"
 dataset2_df = read_csv(url)
 dataset2_df.columns = ['x1', 'x2', 'x3', 'f(x1,x2,x3)']
 
 print(dataset2_df)
 
 #Importing Dataset #3
-url = "https://raw.githubusercontent.com/casigl/Datasets/master/dataset3.csv"
+url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset3.csv"
 dataset3_df = read_csv(url)
 dataset3_df.columns = ['x', 'f(x)']
 
