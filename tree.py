c460e5427e019e9925085cceeab57aa608d1706b
Lindy Bustabad <libustabad@davidson.edu>
2020-09-22 13:44:04 -0400

    attempt lol

---
+++ tree.py
@@ -0,0 +1,33 @@
+import operator
+import random
+
+
+operators = [('+', operator.add), ('-', operator.sub), ('*', operator.mul), ('/', operator.truediv)]
+terminals = [-2, -1, 1, 2, 'x']
+
+class Tree:
+    def __init__(self, body):
+        self.body = body
+        self.left = None
+        self.right = None
+        # flag for operators to distinguish from operands
+        self.operator = False
+
+    def __str__(self):
+        return str(self.body)
+
+def print_tree(tree):
+    if tree == None: return 0
+    print_tree(tree.left)
+    print(tree.body)
+    print_tree(tree.right)
+
+def build_individual():
+    individual = Tree(random.choice(operators))
+    
+
+def main():
+    build_individual()
+
+if __name__ == "__main__":
+    main()
