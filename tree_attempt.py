b7c6cd989e47935d5a9957264397a94da8f97b3b
Lindy Bustabad <libustabad@davidson.edu>
2020-09-29 10:45:16 -0400

    added evaluate method

--- tree_attempt.py
+++ tree_attempt.py
@@ -3,14 +3,14 @@ import random
 import copy
 
 MIN_DEPTH = 2    # minimal initial random tree depth
-MAX_DEPTH = 2    # maximal initial random tree depth
+MAX_DEPTH = 4    # maximal initial random tree depth
 
 def add(x, y): return x + y
 def sub(x, y): return x - y
 def mul(x, y): return x * y
-operators = [add, sub, mul]
-#operators = [('+', operator.add), ('-', operator.sub), ('*', operator.mul), ('/', operator.truediv)]
-terminals = [-2, -1, 1, 2, 'x']
+def div(x, y): return x / y
+operators = [add, sub, mul, div]
+terminals = [-2, -1, 1, 2, 'x','x','x']
 
 class Tree:
     def __init__(self, body = None, left = None, right = None):
@@ -38,10 +38,10 @@ class Tree:
             elif depth >= max_depth:
                 self.body = random.choice(terminals)
             else: # intermediate depth, grow
-                if random.random() > 0.5:
-                    self.body = random.choice(terminals)
-                else:
+                if random.random() > 0.9:
                     self.body = random.choice(operators)
+                else:
+                    self.body = random.choice(terminals)
             if self.body in operators:
                 self.left = Tree()
                 self.left.random_tree(grow, max_depth, depth = depth + 1)
@@ -49,13 +49,33 @@ class Tree:
                 self.right.random_tree(grow, max_depth, depth = depth + 1)
 
     def copy(self):
-      
         return copy.deepcopy(self)
 
+    def evaluate(self):
+        if self.body is None: 
+            return 0   
+        # leaf node 
+        if self.left is None and self.right is None: 
+            return self.body
+        # evaluate left tree 
+        left_sum = self.left.evaluate() 
+        # evaluate right tree 
+        right_sum = self.right.evaluate() 
+        # check which operation to apply 
+        if self.body == add: 
+            return str(left_sum) + '+' + str(right_sum)
+        elif self.body == sub: 
+            return str(left_sum) + '-' + str(right_sum)
+        elif self.body == mul: 
+            return str(left_sum) + '*' + str(right_sum)
+        else: 
+            return str(left_sum) + '/' + str(right_sum)
+        
 def main():
         t = Tree()
         t.random_tree(grow = True, max_depth = MAX_DEPTH, depth = 0)
         t.print_tree()
+        print(t.evaluate())
 
 if __name__ == "__main__":
     main()
