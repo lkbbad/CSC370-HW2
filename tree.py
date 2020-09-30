5805dc9705c01fae53ad65c813db493ada61af09
Lindy Bustabad <libustabad@davidson.edu>
2020-09-29 21:47:40 -0400

    updated tree class

--- tree.py
+++ tree.py
@@ -2,8 +2,9 @@ import operator
 import random
 import copy
 
+POP_SIZE = 1000   # population size
 MIN_DEPTH = 2    # minimal initial random tree depth
-MAX_DEPTH = 4    # maximal initial random tree depth
+MAX_DEPTH = 3   # maximal initial random tree depth
 
 def add(x, y): return x + y
 def sub(x, y): return x - y
@@ -17,6 +18,7 @@ class Tree:
         self.body  = body
         self.left  = left
         self.right = right
+        self.fitness = 0
 
     def __str__(self):
         return str(self.body)
@@ -51,31 +53,54 @@ class Tree:
     def copy(self):
         return copy.deepcopy(self)
 
-    def evaluate(self):
+    def tree_string(self):
         if self.body is None: 
             return 0   
         # leaf node 
         if self.left is None and self.right is None: 
             return self.body
         # evaluate left tree 
-        left_sum = self.left.evaluate() 
+        left_sum = self.left.tree_string() 
         # evaluate right tree 
-        right_sum = self.right.evaluate() 
+        right_sum = self.right.tree_string() 
         # check which operation to apply 
         if self.body == add: 
-            return str(left_sum) + '+' + str(right_sum)
+            return str(left_sum) + '+' + str(right_sum) 
         elif self.body == sub: 
-            return str(left_sum) + '-' + str(right_sum)
+            return str(left_sum) + '-' +  str(right_sum)
         elif self.body == mul: 
-            return str(left_sum) + '*' + str(right_sum)
+            return str(left_sum) + '*' +  str(right_sum) 
         else: 
-            return str(left_sum) + '/' + str(right_sum)
+            return str(left_sum) + '/' +  str(right_sum) 
+
+    def compute_tree(self, x): 
+        if (self.body in operators): 
+            try:
+                return self.body(self.left.compute_tree(x), self.right.compute_tree(x))
+            except:
+                return None
+        elif self.body == 'x': return x
+        else: return self.body
+
+def init_population(): # ramped half-and-half
+    population = []
+    for md in range(3, MAX_DEPTH + 1):
+        for i in range(int(POP_SIZE/6)):
+            t = Tree()
+            t.random_tree(grow = True, max_depth = md) # grow
+            population.append(t) 
+        for i in range(int(POP_SIZE/6)):
+            t = Tree()
+            t.random_tree(grow = False, max_depth = md) # full
+            population.append(t) 
+    return population
         
 def main():
         t = Tree()
         t.random_tree(grow = True, max_depth = MAX_DEPTH, depth = 0)
         t.print_tree()
-        print(t.evaluate())
+        print(t.tree_string())
+        print(t.compute_tree(3))
 
 if __name__ == "__main__":
     main()
