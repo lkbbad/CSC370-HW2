5aea76ba2f288e889588ed1b2b5e3a4ea7a54b07
Lindy Bustabad <libustabad@davidson.edu>
2020-09-29 22:49:37 -0400

    idk what is up

--- gp.py
+++ gp.py
@@ -11,7 +11,7 @@ def fitness(func, x_training, y_training):
     for x in range(len(x_training)):
         diff_squared = 0
         try:
-            diff_squared = (func.compute_tree(x_training[x]) - y_training[x])**2
+            diff_squared = abs(y_training[x]) - (func.compute_tree(x_training[x]))**2
         except:
             func.fitness = float("inf")
             return
@@ -23,10 +23,17 @@ def main():
     # Create population of 1000 trees/functions in a list
     population= tree.init_population() 
     dataset1 = read_in.dataset1()
+    fitnesses = []
+    best_fit = 0
     x_training = dataset1['x'].tolist()
     y_training = dataset1['f(x)'].tolist()
     for func in population:
         fitness(func, x_training, y_training)
+        fitnesses.append(func.fitness)
+    if max(fitnesses) > best_fit:
+        best_fit = max(fitnesses)
+    print(population[fitnesses.index(max(fitnesses))].print_tree())
+    print(population[fitnesses.index(max(fitnesses))].fitness)
 
 if __name__ == "__main__":
     main()
