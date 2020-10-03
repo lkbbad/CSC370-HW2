c2c0e770408ea16dd95a28b8c9f0f981868c562a
Lindy Bustabad <libustabad@davidson.edu>
2020-10-03 14:19:27 -0400

    removed dataset3

--- read_in.py
+++ read_in.py
@@ -9,25 +9,10 @@ from pandas import read_csv
 def dataset1():
     #Importing Dataset #1
     dataset1_df = pandas.read_csv("./dataset1.csv", sep=',', delimiter=None, header='infer')
-    # url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset1.csv"
-    # dataset1_df = read_csv(url)
-    # dataset1_df.columns = ['x', 'f(x)']
     return dataset1_df
 
-# def dataset2():
+def dataset2():
     # #Importing Dataset #2
-    # dataset2_df = pandas.read_csv("./dataset2.csv", sep=',', delimiter=None, header='infer')
-    # url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset2.csv"
-    # dataset2_df = read_csv(url)
-    # dataset2_df.columns = ['x1', 'x2', 'x3', 'f(x1,x2,x3)']
+    dataset2_df = pandas.read_csv("./dataset2.csv", sep=',', delimiter=None, header='infer')
+    return dataset2_df
 
-    # print(dataset2_df)
-
-# def dataset3():
-    # #Importing Dataset #3
-    # dataset3_df = pandas.read_csv("./dataset3.csv", sep=',', delimiter=None, header='infer')
-    # url = "https://raw.githubusercontent.com/lkbbad/CSC370-HW2/master/dataset3.csv"
-    # dataset3_df = read_csv(url)
-    # dataset3_df.columns = ['x', 'f(x)']
-
-    # print(dataset3_df)
