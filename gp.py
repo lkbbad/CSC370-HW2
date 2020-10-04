1e1bb71ab5fc99b8226d6c453f601fd4cb286492
Lindy Bustabad <libustabad@davidson.edu>
2020-10-04 18:36:53 -0400

    lindy

--- gp.py
+++ gp.py
@@ -4,29 +4,53 @@ from statistics import mean
 import sys
 import random
 import copy
+import math
 
 TOURNAMENT_SIZE = 5
 GENERATIONS = 50
-POP_SIZE = 1000 
-
-def fitness(func, x_training, y_training):
-    func.fitness = 1 / (mean([abs(func.compute_tree(x_training[i]) - y_training[i]) for i in range(0, len(x_training)-1)]))
-
-def selection(population, fitnesses): # select one individual using tournament selection
-    tournament = [random.randint(0, len(population)-1) for i in range(TOURNAMENT_SIZE)] # select tournament contenders
-    tournament_fitnesses = [fitnesses[tournament[i]] for i in range(TOURNAMENT_SIZE)]
-    return copy.deepcopy(population[tournament[tournament_fitnesses.index(max(tournament_fitnesses))]]) 
+POP_SIZE = 10
+PROB_MUTATION = 0.1  # probability of perfoming a mutation
+CROSSOVER_RATE = 0.9  # crossover rate
 
 def set_up_data(dataset):
     if dataset == 1:
         return read_in.dataset1()
     else: return read_in.dataset2()
 
+def fitness(func, x_training, y_training):
+    fitness = 0.0
+    for i in range(len(x_training)):
+        diff_squared = (func.compute_tree(x_training[i]) - y_training[i])**2
+        fitness += diff_squared / len(x_training)
+    if not (math.isnan(fitness)):
+        return 1.0 / fitness
+    else: return float("inf")
+
+# def selection(population, fitnesses): # select one individual using tournament selection
+#     tournament = [random.randint(0, len(population)-1) for i in range(TOURNAMENT_SIZE)] # select tournament contenders
+#     tournament_fitnesses = [fitnesses[tournament[i]] for i in range(TOURNAMENT_SIZE)]
+#     return copy.deepcopy(population[tournament[tournament_fitnesses.index(max(tournament_fitnesses))]]) 
+
+def fitness_prop_selection(fitnesses, population):
+    sum_fitness = 0
+    for f in fitnesses:
+        sum_fitness += f.fitness
+    roulette = {f : (f.fitness / sum_fitness) for f in fitnesses}
+
+    max = sum(roulette.values())
+    pick = random.uniform(0, max)
+    current = 0
+    for tree, fitness in roulette.items():
+        current += fitness
+        if current > pick:
+            return tree
+
+
 def dataset1(population, training_df, check_df, TRAINING):
     fitnesses = []
-    best_of_run = None
-    best_of_run_f = 0
-    best_of_run_gen = 0
+    # best_of_run = None
+    # best_of_run_f = 0
+    # best_of_run_gen = 0
 
     x_training = training_df['x'].tolist()
     y_training = training_df['f(x)'].tolist()
@@ -35,8 +59,12 @@ def dataset1(population, training_df, check_df, TRAINING):
 
     # Find fitness for each function in first generation
     for func in population:
-        fitness(func, x_training, y_training)
-        fitnesses.append(func.fitness)
+        func.fitness = fitness(func, x_training, y_training)
+        if func.fitness != float("inf"): 
+            fitnesses.append(func)
+    # for f in fitnesses:
+    #     print(f.fitness)
+
     # if max(fitnesses) > best_fit:
     #     best_fit = max(fitnesses)
 
@@ -50,18 +78,24 @@ def dataset1(population, training_df, check_df, TRAINING):
     for gen in range(1):        
         nextgen_population=[]
         for i in range(1):
-            parent1 = selection(population, fitnesses)
-            print("original")
-            parent1.print_tree()
-            parent2 = selection(population, fitnesses)
-            print("original2")
-            parent2.print_tree()
-            parent1.crossover(parent2)
-            print("cross")
-            parent1.print_tree()
-            parent1.mutation()
-            print("mutate")
-            parent1.print_tree()
+            xo_parent_num = int(int(POP_SIZE / 2) * CROSSOVER_RATE)
+            mut_parent_num = int(POP_SIZE - (2 * xo_parent_num))
+            for j in range(xo_parent_num):
+                xo_parent1 = fitness_prop_selection(fitnesses, population)
+                xo_parent2 = fitness_prop_selection(fitnesses, population)
+                print(xo_parent1, xo_parent2)
+            for k in range(mut_parent_num):
+                mut_parent = fitness_prop_selection(fitnesses, population)
+                print(mut_parent)
+
+    #         parent1 = selection(population, fitnesses)
+    #         parent1.print_tree()
+    #         parent2 = selection(population, fitnesses)
+    #         parent2.print_tree()
+    #         parent1.crossover(parent2)
+    #         parent1.print_tree()
+    #         parent1.mutation()
+    #         parent1.print_tree()
     #         nextgen_population.append(parent1)
     #     population=nextgen_population
     #     fitnesses = [fitness(population[i], x_training, y_training) for i in range(POP_SIZE)]
--- tree.py
+++ tree.py
@@ -2,17 +2,17 @@ import operator
 import random
 import copy
 
-POP_SIZE = 1000   # population size
+POP_SIZE = 10   # population size
 MIN_DEPTH = 2    # minimal initial random tree depth
 MAX_DEPTH = 4   # maximal initial random tree depth
-PROB_MUTATION = 0.2  # probability of perfoming a mutation
+PROB_MUTATION = 0.75  # probability of perfoming a mutation
 CROSSOVER_RATE = 0.8  # crossover rate
 
 def add(x, y): return x + y
 def sub(x, y): return x - y
 def mul(x, y): return x * y
 def div(x, y): return x / y
-operators = [add, sub, mul]
+operators = [add, sub, mul, div]
 terminals = [-2, -1, 1, 2, 'x','x','x']
 
 class Tree:
@@ -21,6 +21,7 @@ class Tree:
         self.left  = left
         self.right = right
         self.fitness = 0
+        # self.count = count(self)
 
     def __str__(self):
         return str(self.body)
@@ -36,10 +37,10 @@ class Tree:
         if self.left:  self.left.print_tree (prefix + "   ")
         if self.right: self.right.print_tree(prefix + "   ")
 
-    def random_tree(self, grow, max_depth, depth = 0): # create random tree using either grow or full method
-            if depth < MIN_DEPTH or (depth < max_depth and not grow):
+    def random_tree(self, grow, max_depth, depth = 0): 
+            if depth < MIN_DEPTH or (depth < MAX_DEPTH and not grow):
                 self.body = random.choice(operators)
-            elif depth >= max_depth:
+            elif depth >= MAX_DEPTH:
                 self.body = random.choice(terminals)
             else: # intermediate depth, grow
                 if random.random() > 0.9:
@@ -80,70 +81,106 @@ class Tree:
             try:
                 return self.body(self.left.compute_tree(x), self.right.compute_tree(x))
             except:
-                return None
+                return float("inf")
         elif self.body == 'x': return x
         else: return self.body
         
     def mutation(self):
-        if random.random() < PROB_MUTATION: # mutate at this node
-            self.random_tree(grow = True, max_depth = 1)
+        rand = random.uniform(0,1)
+        if rand > PROB_MUTATION or (not self.left and not self.right): # mutate at this node
+            if self.body in operators:
+                new = random.choice(operators)
+                if new != self.body:
+                    self.body = new
+            else: 
+                new = random.choice(terminals)
+                if new != self.body:
+                    self.body = new
         elif self.left: self.left.mutation()
         elif self.right: self.right.mutation()
+
         
-    def size(self): # tree size in nodes
-        if self.body in operators: return 1
-        l = self.left.size()  if self.left  else 0
-        r = self.right.size() if self.right else 0
-        return 1 + l + r
+    # def size(self): # tree size in nodes
+    #     if self.body in operators: return 1
+    #     l = self.left.size()  if self.left  else 0
+    #     r = self.right.size() if self.right else 0
+    #     return 1 + l + r
 
-    def build_subtree(self): # count is list in order to pass "by reference"
-        t = Tree()
-        t.body = self.body
-        if self.left:  t.left  = self.left.build_subtree()
-        if self.right: t.right = self.right.build_subtree()
-        return t
+    # def build_subtree(self): # count is list in order to pass "by reference"
+    #     t = Tree()
+    #     t.body = self.body
+    #     if self.left:  t.left  = self.left.build_subtree()
+    #     if self.right: t.right = self.right.build_subtree()
+    #     return t
                         
-    def scan_tree(self, count, second): # note: count is list, so it's passed "by reference"
-        count[0] -= 1
-        if count[0] <= 1:
-            if not second: # return subtree rooted here
-                return self.build_subtree()
-            else: # glue subtree here
-                self.body  = second.body
-                self.left  = second.left
-                self.right = second.right
-        else:
-            ret = None
-            if self.left  and count[0] > 1: ret = self.left.scan_tree(count, second)
-            if self.right and count[0] > 1: ret = self.right.scan_tree(count, second)
-            return ret
+    # def scan_tree(self, count, second): # note: count is list, so it's passed "by reference"
+    #     count[0] -= 1
+    #     if count[0] <= 1:
+    #         if not second: # return subtree rooted here
+    #             return self.build_subtree()
+    #         else: # glue subtree here
+    #             self.body  = second.body
+    #             self.left  = second.left
+    #             self.right = second.right
+    #     else:
+    #         ret = None
+    #         if self.left  and count[0] > 1: ret = self.left.scan_tree(count, second)
+    #         if self.right and count[0] > 1: ret = self.right.scan_tree(count, second)
+    #         return ret
 
-    def crossover(self, other): # xo 2 trees at random nodes
-        if random.random() < CROSSOVER_RATE:
-            second = other.scan_tree([random.randint(1, other.size())], None) # 2nd random subtree
-            self.scan_tree([random.randint(1, self.size())], second) # 2nd subtree "glued" inside 1st tree
-        else :
-            print("no crossover")
+    # def crossover(self, other): # xo 2 trees at random nodes
+    #     if random.random() < CROSSOVER_RATE:
+    #         second = other.scan_tree([random.randint(1, other.size())], None) # 2nd random subtree
+    #         self.scan_tree([random.randint(1, self.size())], second) # 2nd subtree "glued" inside 1st tree
+    #     else :
+    #         print("no crossover")
 
 
 def init_population(): # ramped half-and-half
     population = []
-    for md in range(3, MAX_DEPTH + 1):
-        for i in range(int(POP_SIZE/6)):
+    for md in range(MIN_DEPTH, MAX_DEPTH - 1):
+        for i in range(int(POP_SIZE/ 2)):
             t = Tree()
             t.random_tree(grow = True, max_depth = md) # grow
             population.append(t) 
-        for i in range(int(POP_SIZE/6)):
+        for i in range(int(POP_SIZE/ 2)):
             t = Tree()
             t.random_tree(grow = False, max_depth = md) # full
             population.append(t) 
     return population
+
+# def count(tree): # counts the number of nodes in tree
+#     if not tree: return 0
+#     return count(tree.left) + 1 + count(tree.right)
+
+# # function to get the random node
+# def get_random_node(tree):
+#     a = tree.count
+#     b = tree.left.count if tree.left else 0
+#     c = tree.right.count if tree.right else 0
+    
+#     if b == c == 0:
+#         return tree.body
+    
+#     rand_num = random.randint(1, a)
+    
+#     if rand_num <= b: return get_random_node(tree.left)
+#     elif rand_num == b + 1: return tree.val
+#     else: return get_random_node(tree.right)
+
+
         
-# def main():
-        # t1 = Tree()
-        # t1.random_tree(grow = True, max_depth = MAX_DEPTH, depth = 0)
-        # t1.print_tree()
+def main():
+    t1 = Tree()
+    t1.random_tree(grow = True, max_depth = MAX_DEPTH, depth = 0)
+    t1.print_tree()
+    # print(count(t1))
+    # print(get_random_node(t1))
+    t1.mutation()
+    t1.print_tree()
+
         # print(t1.tree_string())
+
         # print(t1.compute_tree(3))
         
         # demonstrating mutation
